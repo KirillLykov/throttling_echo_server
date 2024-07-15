ad9e3810ff6d9300cbed4d8cfff65cfa8d61b142
Kirill Lykov <kirill.lykov@anza.xyz>
2024-07-15 13:06:49 +0200

    add sender/receiver for data

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ use {
         sync::Arc,
     },
     tokio::{
-        sync::mpsc,
+        sync::mpsc::{self, unbounded_channel, UnboundedReceiver, UnboundedSender},
         time::{self, Duration, Instant},
     },
 };
+
+type Sender = UnboundedSender<Vec<u8>>;
 
 async fn produce_tokens(
     bytes_per_token: usize,
@@ -29,22 +31,32 @@ async fn produce_tokens(
     Ok(())
 }
 
-async fn transfer(mut from: quinn::RecvStream, mut tokens: mpsc::Receiver<usize>) -> Result<()> {
+async fn transfer(
+    mut from: quinn::RecvStream,
+    mut tokens: mpsc::Receiver<usize>,
+    sender: Sender,
+) -> Result<()> {
     // TODO try using Bytes: BytesMut::with_capacity(4096);
-    let mut data = vec![0u8; 4096];
+    const BUF_SIZE: usize = 4 * 1024;
+    let mut data = vec![0u8; BUF_SIZE];
     loop {
         let Some(mut bytes_available) = tokens.recv().await else {
             break;
         };
 
         while bytes_available > 0 {
-            match from.read(&mut data[..bytes_available.min(4096)]).await? {
+            match from
+                .read(&mut data[..bytes_available.min(BUF_SIZE)])
+                .await?
+            {
                 Some(n_read) => {
                     eprintln!("Read chunk: {:?}", &data[..n_read]);
+                    sender.send(data[..n_read].to_vec())?;
                     bytes_available -= n_read;
                 }
                 None => {
                     eprintln!("Stream finished.");
+                    break;
                 }
             }
         }
@@ -52,23 +64,33 @@ async fn transfer(mut from: quinn::RecvStream, mut tokens: mpsc::Receiver<usize>
     Ok(())
 }
 
-async fn proxy(incoming: Incoming) -> Result<()> {
+async fn proxy(incoming: Incoming, sender: Sender) -> Result<()> {
     const NUMBER_OF_TOKENS: usize = 100;
     const BYTES_PER_TOKEN: usize = 20 * 1024;
     const TOKEN_INTERVAL: Duration = Duration::from_millis(100);
 
     let connection = incoming.await?;
 
-    let listener_recv = connection.accept_uni().await?;
+    loop {
+        let stream = connection.accept_uni().await;
+        let stream = match stream {
+            Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+                eprintln!("connection closed");
+                return Ok(());
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+            Ok(s) => s,
+        };
 
-    let (listener_token_sender, listener_token_receiver) = mpsc::channel(NUMBER_OF_TOKENS);
+        let (listener_token_sender, listener_token_receiver) = mpsc::channel(NUMBER_OF_TOKENS);
 
-    tokio::try_join!(
-        produce_tokens(BYTES_PER_TOKEN, TOKEN_INTERVAL, listener_token_sender),
-        transfer(listener_recv, listener_token_receiver),
-    )?;
-
-    Ok(())
+        tokio::try_join!(
+            produce_tokens(BYTES_PER_TOKEN, TOKEN_INTERVAL, listener_token_sender),
+            transfer(stream, listener_token_receiver, sender.clone()),
+        )?;
+    }
 }
 
 /// Returns default server configuration along with its certificate.
@@ -85,7 +107,7 @@ fn configure_server() -> Result<(ServerConfig, CertificateDer<'static>)> {
     Ok((server_config, cert_der))
 }
 
-async fn listen(listen: SocketAddr) -> Result<()> {
+async fn listen(listen: SocketAddr, sender: Sender) -> Result<()> {
     let Ok((server_config, _)) = configure_server() else {
         return Err(anyhow!("This is a custom error message"));
     };
@@ -94,7 +116,14 @@ async fn listen(listen: SocketAddr) -> Result<()> {
 
     while let Some(incoming) = endpoint.accept().await {
         eprintln!("accepting connection");
-        tokio::spawn(proxy(incoming));
+        tokio::spawn(proxy(incoming, sender.clone()));
+    }
+    Ok(())
+}
+
+async fn consume_data(mut receiver: UnboundedReceiver<Vec<u8>>) -> Result<()> {
+    while let Some(data) = receiver.recv().await {
+        eprintln!("{data:?}")
     }
     Ok(())
 }
@@ -114,7 +143,8 @@ struct Cli {
 async fn main() -> Result<()> {
     let args = Cli::parse();
 
-    listen(args.listen_address).await?;
+    let (sender, receiver) = unbounded_channel();
 
+    tokio::try_join!(listen(args.listen_address, sender), consume_data(receiver))?;
     Ok(())
 }
